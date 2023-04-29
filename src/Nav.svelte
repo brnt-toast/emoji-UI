2699a07d9ff1b64125bbc438dc1265e5a3f8cbe5
brnt-toast <yoyo.beerme@gmail.com>
2023-04-29 15:03:08 -0400

    added: global styles and Nav.svelte

---
+++ src/Nav.svelte
@@ -0,0 +1,38 @@
+
+
+<nav>
+    <a href="/">
+        🏠
+    </a>
+
+
+    <div class="button_group">
+
+        <button class="signup">
+            Sign Up
+        </button>
+        <button>    
+            ⚙️
+        </button>
+    </div>
+</nav>
+
+<style>
+    nav {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 1rem 0;
+    }
+
+    button {
+        background-color: transparent;
+        border: none;
+        padding: 1rem;
+        border-radius: .5rem;
+    }
+
+    .signup {
+        background-color: aqua;
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    import "../main.css"
+    import Nav from '../Nav.svelte'
+</script>
+
+<Nav />
+
+<slot></slot>
