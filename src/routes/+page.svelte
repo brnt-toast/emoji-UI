d083671b483d734fb227438365b47aca25aad330
brnt-toast <yoyo.beerme@gmail.com>
2023-05-03 17:20:47 -0400

    new homepage message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>🐻 Only 🫵 Can Prevent WildFires 🐻</h1>
+
+<style>
+    h1 {
+        text-align: center;
+    }
+</style>
