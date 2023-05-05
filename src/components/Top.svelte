85acae7cfd15bddf7da2393a5a235983190445d3
brnt-toast <yoyo.beerme@gmail.com>
2023-05-05 01:11:41 -0400

    init: <Top />

---
+++ src/components/Top.svelte
@@ -0,0 +1,14 @@
+<button>
+    🔝
+</button>
+
+<style>
+    button {
+        position: fixed;
+        right: 1rem;
+        bottom: 1rem;
+        padding: .5rem;
+        background-color: var(--yellow);
+        font-size: 2rem;
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script>
     import "../main.css"
     import Nav from '../components/Nav.svelte'
+    import Top from '../components/Top.svelte'
 </script>
 
 <Nav />
+<Top />
 
 <slot></slot>
